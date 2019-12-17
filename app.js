fb7b3ac620b28baf4434194b3a7f96dad887d79d
Gentian Shkembi <gentian.shkembi@gmail.com>
2019-12-17 15:00:58 +0100

    Added styling and refactored code

--- app.js
+++ app.js
@@ -6,77 +6,67 @@ const message = document.getElementById('message');
 const hint = document.getElementById('hint');
 const newGameBtn = document.getElementById('new-game');
 
-const generateRandomNumber = () => {
-    return Math.round(Math.random() * 100);
+class GuessingGame {
+    turn = 1;
+    randomNumber = this.generateRandomNumber();
+    guessesList = [];
+
+    generateRandomNumber() {
+        return Math.round(Math.random() * 100);
+    }
 }
-// const init = () => {
-//     let turn = 1;
-//     let guessesList = [];
-// }
-let randomNumber = generateRandomNumber();
+let guessingGame = new GuessingGame();
 
-let turn = 1;
-
-let guessesList = [];
-
-// let messages = {
-//     wrongGuess: 'Wrong!',
-//     gameOver: '!!!GAME OVER!!!',
-//     win: 'Congratulations! You got it right!',
-//     lowerGuess: 'Last guess was too low!',
-//     greaterGuess: 'Last guess was too high!'
-// }
-const wrongGuess = 'Wrong!';
-const gameOver = '!!!GAME OVER!!!';
-const win = 'Congratulations! You got it right!';
-const lowerGuess = 'Last guess was too low!';
-const greaterGuess = 'Last guess was too high!'
+let messages = {
+    wrongGuess: '<p class="alert alert-danger">Wrong!</p>',
+    gameOver: '<p class="alert alert-danger">!!!GAME OVER!!!</p>',
+    win: '<p class="alert alert-success">Congratulations! You got it right!</p>',
+    lowerGuess: '<p class="alert alert-primary">Last guess was too low!</p>',
+    greaterGuess: '<p class="alert alert-primary">Last guess was too high!</p>'
+}
 
 submitGuessedNumber.onclick = () => {
-    console.log(randomNumber)
-
     if (!guessedNumber.value) {
         guessedNumber.value = 0;
     }
     let currentNumber = Number(guessedNumber.value)
-    guessesList.push(Number(currentNumber));
-    console.log(guessesList)
-    if (guessesList.length) {
+    guessingGame.guessesList.push(Number(currentNumber));
+    if (guessingGame.guessesList.length) {
         guessList.style.display = 'block';
-        guessedNumbersList.append(currentNumber);
+        guessedNumbersList.append(currentNumber + ' ');
     }
 
-    if (currentNumber > randomNumber) {
-        message.innerHTML = wrongGuess;
-        hint.innerHTML = greaterGuess;
-    } else if (currentNumber < randomNumber) {
-        message.innerHTML = wrongGuess;
-        hint.innerHTML = lowerGuess;
+    if (currentNumber > guessingGame.randomNumber) {
+        message.innerHTML = messages.wrongGuess;
+        hint.innerHTML = messages.greaterGuess;
+    } else if (currentNumber < guessingGame.randomNumber) {
+        message.innerHTML = messages.wrongGuess;
+        hint.innerHTML = messages.lowerGuess;
     } else {
         submitGuessedNumber.setAttribute('disabled', true);
-        message.innerHTML = win;
+        message.innerHTML = messages.win;
         hint.innerHTML = '';
         newGameBtn.style.display = 'block';
     }
 
     guessedNumber.value = '';
-    turn++;
-    if (turn > 10 && message.innerHTML !== win) {
-        message.innerHTML = gameOver;
+    guessingGame.turn++;
+    if (guessingGame.turn > 10 && message.innerHTML !== messages.win) {
+        message.innerHTML = messages.gameOver;
         submitGuessedNumber.setAttribute('disabled', true);
         hint.innerHTML = '';
         newGameBtn.style.display = 'block';
     }
+    console.log(guessingGame)
+
 }
 
 newGameBtn.onclick = () => {
-    guessesList = [];
     message.innerHTML = '';
     hint.innerHTML = '';
     submitGuessedNumber.removeAttribute('disabled');
     newGameBtn.style.display = 'none';
     guessList.style.display = 'none';
     guessedNumbersList.innerHTML = '';
-    turn = 1;
-    randomNumber = generateRandomNumber();
+    guessingGame = new GuessingGame();
 }
